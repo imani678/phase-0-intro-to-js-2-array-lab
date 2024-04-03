abecf37922093772ae36b72eb323b51f50d09a81
imani <18imani.vata@gmail.com>
2024-04-03 11:31:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats= ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendCat (){
+    cats.push('Ralph')
+    console.log (cats)
+}
+
+function destructivelyPrependCat (){
+    cats.unshift ('Bob')
+    console.log (cats)
+}
+
+function destructivelyRemoveLastCat (){
+    cats.pop();
+    console.log (cats)
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+   const newArray=cats.slice()
+   newArray.push(name) 
+   return newArray
+}
+
+function prependCat(name){
+    const newArray=cats.slice()
+    newArray.unshift(name)
+    return newArray
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
